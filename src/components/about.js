a6eb0b259e73ce46ef791a87bbaf72753f73c96a
half6 <christofferhalf6@gmail.com>
2023-05-12 09:56:23 +0200

    moved from noroff repo to my own

--- src/components/about.js
+++ src/components/about.js
@@ -1,6 +1,12 @@
+import gsap from 'gsap'
 const buttons = document.querySelectorAll('.img-slider-button');
 const slides = document.querySelector('#slides');
+const body = document.querySelector('body')
+const slideImages = document.querySelector('.image-slider');
+const modalBtn = document.querySelector( '.modal-btn')
+const postSection = document.querySelector('.post-section')
 
+const modal = document.querySelector('.about-modal')
 buttons.forEach(button => {
   button.addEventListener('click', () => {
     const findNext = button.classList.contains('next') ? 1 : -1;
@@ -17,4 +23,34 @@ buttons.forEach(button => {
     slides.children[newIndex].classList.add('active-slide');
   });
 });
+
+const imageSlides = document.querySelectorAll('.image-carousel .slide');
+
+const modalImage = modal.querySelector('img');
+
+imageSlides.forEach((slide) => {
+  slide.addEventListener('click', () => {
+    const clickedImageSrc = slide.querySelector('img').getAttribute('src');
+    modalImage.setAttribute('src', clickedImageSrc);
+    modal.showModal();
+    body.classList.add('stop-scrolling')
+  });
+});
+
+const dialog = document.querySelector('dialog');
+dialog.addEventListener("click", (event) => {
+
+    if (event.target === dialog) {
+   
+      dialog.close();
+      
+      body.classList.remove('stop-scrolling')
+    }
+  });
+
+  modalBtn.addEventListener('click',()=> {
+    modal.close()
+    const body = document.querySelector('body')
+    body.classList.remove('stop-scrolling')
+})
 
--- src/components/posts.js
+++ src/components/posts.js
@@ -1,5 +1,5 @@
 import FetchHelper from "./fetchHelper.js";
-
+import { gsap } from "gsap";
 const loader = document.querySelector('.spinner')
 const postSection = document.querySelector('.post-section')
 const category = document.querySelector('.category')
@@ -9,10 +9,11 @@ const sortDate  = document.querySelector('.sort-date')
 const searchForm = document.querySelector('.search-form')
 const searchInput = document.querySelector('#search')
 
-let pages = 9
+let pages = 1
 let blogPosts = []
 let total = 0
 let categoriesArr = []
+
 
   async function getCategory(){
     const API = new FetchHelper(`${import.meta.env.VITE_API_KEY2}`)
@@ -32,77 +33,118 @@ async function getData(categoryValue = '', searchQuery =''){
           response  = await API.get(`?_embed&search=${searchQuery}`)
         }
         else {
-          response = await API.get(`?_embed${categoryValue ? categoryValue : ''}&orderby=date&order=desc&per_page=${pages}`)    
+          response = await API.get(`?_embed${categoryValue ? categoryValue : ''}&orderby=date&order=desc&page=${pages}`)    
         }       
         const data = await response.json();
-        total = response.headers.get('x-wp-total');
+        total = response.headers.get('x-wp-totalpages');
         return [data, total]
     }catch(error){
         console.log(error)
     }
 }
 
- function renderHTML(data, totalPosts){
 
-     blogPosts = data
 
+ function renderHTML(data, totalPosts,animate = true){
+
+    blogPosts = data
     const total = Number.parseInt(totalPosts,10)
-    const postArr = Number.parseInt(data.length,10)
-    postSection.textContent = ''
 
-    loadMore.addEventListener('click',  () => {
-        if (postArr < total) {
-          pages += 3;
-          
-          renderPage();
-        } 
-      });
-      if(postArr === total){
+
+    loadMore.addEventListener('click', async () => {
+      if (pages < total) {
+        pages += 1;
+        renderPage('','',false)
+      }
+    });
+
+      if(pages === total){
         loadMore.style.display = 'none'
       }else{
         loadMore.style.display = 'block'
       }
 
+      if(blogPosts.length === 0){
+        const noResults = document.createElement('h2')
+        noResults.textContent = `No results matching "${searchInput.value}" found`
+        noResults.style.textAlign = 'center'
+        postSection.append(noResults)
+       }
+
        
-    blogPosts.map( post => {
+  
+       
+    blogPosts.forEach( post => {
 
 
-                const formatedText = post.excerpt.rendered
-                const parser = new DOMParser();
-                const formatedElement = parser.parseFromString(formatedText, 'text/html').body.firstChild;
-                const formattedFinal = formatedElement.textContent;
+        const formatedText = post.excerpt.rendered
+        const parser = new DOMParser();
+        const formatedElement = parser.parseFromString(formatedText, 'text/html').body.firstChild;
+        const formattedFinal = formatedElement.textContent;
 
-                const articleContainer = document.createElement('div')
-                articleContainer.className = 'article-container'
-                
-                const imgContainer = document.createElement('div')
-                imgContainer.className = 'posts-img-container'
-                const articleImg = document.createElement('img')
-                articleImg.src = `${post._embedded['wp:featuredmedia'][0].source_url}`
-                imgContainer.append(articleImg)
+        const articleContainer = document.createElement('div')
+        articleContainer.className = 'article-container'
+        articleContainer.id = post.id
+        
+        const imgContainer = document.createElement('div')
+        imgContainer.className = 'posts-img-container'
+        const articleImg = document.createElement('img')
+        articleImg.src = `${post._embedded['wp:featuredmedia'][0].source_url}`
+        imgContainer.append(articleImg)
 
-                const titleContainer = document.createElement('div')
-                titleContainer.className = 'article-title-container'
+        const titleContainer = document.createElement('div')
+        titleContainer.className = 'article-title-container'
 
-                const postTitle = document.createElement('a')
-                postTitle.textContent = post.title.rendered
-                postTitle.className = 'title-link'
-                postTitle.href=`details.html?id=${post.id}`
+        const postTitle = document.createElement('a')
+        postTitle.textContent = post.title.rendered
+        postTitle.className = 'title-link'
+        postTitle.href=`details.html?id=${post.id}`
 
-                const postArticle = document.createElement('p')
-                postArticle.textContent = formattedFinal
-                postArticle.className = 'subTitle-text'
+        const postArticle = document.createElement('p')
+        postArticle.textContent = formattedFinal
+        postArticle.className = 'subTitle-text'
 
-                titleContainer.append(postTitle,postArticle)
-                articleContainer.append(imgContainer, titleContainer)            
-                postSection.append(articleContainer)
-            });       
+        titleContainer.append(postTitle,postArticle)
+        articleContainer.append(imgContainer, titleContainer)            
+        postSection.append(articleContainer)
+
+
+
+
+            });
+
+ 
+
+
+            if(animate){
+
+              gsap.fromTo(`.article-container`,
+                {
+                x:'200%'
+                },
+            {
+              x:0,
+              duration:0.5,
+              ease: "power2.out",
+              stagger:{
+                each:0.15,
+                from:'start'
+              }
+            
+            })
+            }
+          
+
+          
+      
 
 }
+
 //filter
 category.addEventListener('change',()=>{
     let found = categoriesArr.find(item => item.name === category.value)
     found ? renderPage(`&categories=${found.id || ''}`,'') : renderPage('','')
+    postSection.textContent = ''
 })
 
 sortDate.addEventListener('change', () => {
@@ -128,23 +170,30 @@ searchForm.addEventListener('submit',(e)=>{
 function search(cleaner){
     const trimmed = cleaner.trim()
     const urlConvert = encodeURIComponent(trimmed);
+    postSection.textContent = ''
     renderPage('',urlConvert)
 }
 
 
 
-async function renderPage(categoryValue = '', searchQuery = ''){
+async function renderPage
+(
+  categoryValue = '', 
+  searchQuery = '', 
+  animate
+)
+  {
     try{
-        loader.classList.add('show')
-        console.log(categoryValue)
 
+        loader.classList.add('show')
         const [data, totalPosts ]= await getData(categoryValue,searchQuery) 
-        renderHTML(data,totalPosts)
+        renderHTML(data,totalPosts,animate)
 
     }catch(error){
         console.log(error)
     }finally{
         loader.classList.remove('show')
+      
     }
 }
 
@@ -152,4 +201,9 @@ window.addEventListener('load',() => {
   category.value = ''
   sortDate.value = 'newest'
 })
+
+
+
+
+
 renderPage()
